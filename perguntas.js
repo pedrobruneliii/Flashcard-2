7dd85b7d0b0578b6485fee0488188ad24576e877
pedrobruneliii <142426275+pedrobruneliii@users.noreply.github.com>
2024-11-18 11:48:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Copa do Mundo',
+    'Em que ano foi realizada a primeira Copa do Mundo da FIFA?',
+    'A primeira Copa do Mundo da FIFA foi realizada em 1930.'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual país sediou a primeira edição da Copa do Mundo?',
+    'O Uruguai sediou a primeira edição da Copa do Mundo'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual seleção detém o maior número de títulos da Copa do Mundo?',
+    ' O Brasil detém o maior número de títulos, com um total de 5 conquistas.'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Quem foi o artilheiro da Copa do Mundo de 2006?',
+    'O artilheiro da Copa do Mundo de 2006 foi Miroslav Klose, da Alemanha, com 5 gols.'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Quantas seleções participaram da Copa do Mundo de 2018?',
+    'A Copa do Mundo de 2018 teve a participação de 32 seleções.'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Em que ano a Copa do Mundo foi realizada pela primeira vez em solo africano?',
+    'A Copa do Mundo foi realizada pela primeira vez na África em 2010, na África do Sul.'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual é a seleção que venceu a Copa do Mundo de 2014?',
+    'A Alemanha venceu a Copa do Mundo de 2014'
+)
+
+
+criaCartao(
+    'Copa do Mundo',
+    'Qual foi o resultado da final da Copa do Mundo de 1998?',
+    'Na final da Copa do Mundo de 1998, a França venceu o Brasil por 3 a 0.'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
